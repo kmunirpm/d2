b4d9b187ea64bc8d6d826ebddb2b913dffb53ce7
Kashif Munir <77894778+kmunirpm@users.noreply.github.com>
2021-11-02 23:29:06 -0400

    timer1

--- clock/timer1.js
+++ clock/timer1.js
@@ -1,9 +1,15 @@
-setTimeout(() => {
-  console.log("4311o")
-}, 1000)
-setTimeout(() => {
-  console.log("th3r3")
-}, 2000)
-setTimeout(() => {
-  console.log("w0r1d")
-}, 3000)
+let arr = [];
+for (let j = 2; j < process.argv.length; j++) {
+  if(Number(process.argv[j]) && Number(process.argv[j]) > 0)
+    arr.push(process.argv[j]);
+}
+arr.sort()
+
+const beep = function () {
+  process.stdout.write('\x07');
+}
+
+arr.forEach( val => {
+  setTimeout(beep, val*1000)
+  console.log(val)
+})
